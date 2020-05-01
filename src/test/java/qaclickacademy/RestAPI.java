c01b5539ed165b78911bb1d8b973181157beeadf
igusigus123 <iga.gawinkowska@gmail.com>
2020-05-01 14:18:55 +0200

    commit from GitX

--- src/test/java/qaclickacademy/RestAPI.java
+++ src/test/java/qaclickacademy/RestAPI.java
@@ -6,6 +6,8 @@ public class RestAPI {
 	@Test
 	public void RestApi1() {
 		System.out.println("RestApi1");
+		System.out.println("RestApi2");
+		System.out.println("RestApi3");
 	}
 	@Test
 	public void RestApi2() {
