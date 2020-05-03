793b015b16f924e5df24f4df166defcd62d1ae79
igusigus123 <iga.gawinkowska@gmail.com>
2020-05-03 23:34:28 +0200

    11

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -6,9 +6,7 @@ public class SeleniumTest {
 	
 	@Test
 	public void Selenium1() {
-		System.out.println("Selenium1");
-		System.out.println("Selenium test1 branch developX");
-		System.out.println("Selenium test1 branch developX");
+		System.out.println("Zostaje samo selenium1");
 		
 	}
 	@Test
